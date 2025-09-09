305509df0e219a86d92a320dec4b4a51dde1798b
kaito <kaito.kotake.0920@gmail.com>
2025-09-09 09:40:05 +0900

    18_講義

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,21 +3,35 @@ package raisetech.StudentManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+  private String name = "Kouji ENAMI";
+  private String age = "37";
+
   public static void main(String[] args) {
     //localhost:8080
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "HelloWorld";
+  @GetMapping("/StudentInfo")
+  public String getStudentInfo() {
+    return name + " " + age + "歳";
   }
 
+  @PostMapping("/StudentInfo")
+  public void setStudentInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @PostMapping("/StudentName")
+  public void updateStudentName(String name) {
+    this.name = name;
+  }
 
 }
