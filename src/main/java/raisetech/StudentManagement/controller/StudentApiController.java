64bf21c61d65b55cad57e0190d91271c151bfdcc
kaito <kaito.kotake.0920@gmail.com>
2025-10-20 10:05:30 +0900

    バリデーションの追加

--- src/main/java/raisetech/StudentManagement/controller/StudentApiController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentApiController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import raisetech.StudentManagement.service.StudentService;
 /**
  * 受講生の検索、登録、更新を行うREST APIとして受け付けるController
  */
+@Validated
 @RestController
 public class StudentApiController {
 
