f79298dfc41793a022cbd8ab2ff3ec40a6dbade1
kaito <kaito.kotake.0920@gmail.com>
2025-10-22 14:24:41 +0900

    ドキュメントの作成

--- src/main/java/raisetech/StudentManagement/data/Course.java
+++ src/main/java/raisetech/StudentManagement/data/Course.java
@@ -1,9 +1,11 @@
 package raisetech.StudentManagement.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(description = "コース情報")
 @Getter
 @Setter
 
