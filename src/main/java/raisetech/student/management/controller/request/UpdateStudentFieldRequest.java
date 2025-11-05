595aef0ec15177e4da7888d8a9464ddb1bb1fa13
kaito <kaito.kotake.0920@gmail.com>
2025-11-05 16:43:08 +0900

    PR指摘改善

--- src/main/java/raisetech/student/management/controller/request/UpdateStudentFieldRequest.java
+++ src/main/java/raisetech/student/management/controller/request/UpdateStudentFieldRequest.java
@@ -1,9 +1,6 @@
 package raisetech.student.management.controller.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
